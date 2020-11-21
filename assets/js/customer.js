9f5b09b07049a92490c85d6f9da40387446ba89f
Meera Qunaibe <meeraiyad98@gmail.com>
2020-11-21 17:21:13 +0200

    creating an array for products

---
+++ assets/js/customer.js
@@ -0,0 +1,37 @@
+// let search = document.getElementsByClassName("search-engine");
+// let product = document.getElementsByClassName("item");
+// let list = document.createElement("li");
+// list.appendChild(product);
+// for (i = 0; i < li.length; i++) {
+//     let a = li.product[0];
+//     txtValue = a.textContent;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "block";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+// }
+let productWrapper = document.getElementsByClassName("products")[0];
+console.log(productWrapper);
+let products=[{
+    id: 0, 
+    name: "T-shirt",
+    price: 25,
+    category: "clothes",
+    URL: "./images/coot.jpg"},
+    {
+        id: 1, 
+        name: "T-shirt",
+        price: 35,
+        category: "clothes",
+        URL: "./images/coot.jpg"}];
+    products.forEach(function(product){
+        let card = document.createElement("div");
+        productWrapper.appendChild(card);
+        let productImage = document.createElement("img");
+        productImage.src = product.URL;
+        card.appendChild(productImage);
+    })
+    
+
+

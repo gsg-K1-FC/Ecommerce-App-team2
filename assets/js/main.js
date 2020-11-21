82084c0260a69b22347bdf4ffec200974daded8e
osamaayyad99 <osama.ayyad.99@gmail.com>
2020-11-21 16:59:19 +0200

    make navbar with basic pages

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("sellerbtn").onclick = function(){
-    location.href= "seller.html";
-}
-document.getElementById("customerbtn").onclick = function(){
-    location.href= "customer.html";
-}
-document.getElementById("homebtn").onclick = function(){
-    location.href= "index.html";
-}

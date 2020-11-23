fc4664ea500819a8c64305df5b48135ee3457dce
Meera Qunaibe <meeraiyad98@gmail.com>
2020-11-23 21:30:41 +0200

    searching-for-item-by-name

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -1,47 +1,59 @@
-// let search = document.getElementsByClassName("search-engine");
-// let product = document.getElementsByClassName("item");
-// let list = document.createElement("li");
-// list.appendChild(product);
-// for (i = 0; i < li.length; i++) {
-//     let a = li.product[0];
-//     txtValue = a.textContent;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         li[i].style.display = "block";
-//       } else {
-//         li[i].style.display = "none";
-//       }
-// }
 let productWrapper = document.getElementsByClassName("products")[0];
 console.log(productWrapper);
 let products=[{
     id: 0, 
-    name: "T-shirt",
+    name: "coot",
     price: 25,
-    category: "clothes",
+    category: "Clothes",
     URL: "../../assets/images/coot.jpg"},
     {
         id: 1, 
-        name: "T-shirt",
+        name: "palette",
         price: 35,
-        category: "clothes",
-        URL: "./images/coot.jpg"}];
+        category: "Makeup",
+        URL: "../../assets/images/palette.jpg"},
+        {
+            id: 2, 
+            name: "scarf",
+            price: 35,
+            category: "clothes",
+            URL: "../../assets/images/scarf.webp"}];
+        let cartItem=[];
+
     products.forEach(function(product){
         let card = document.createElement("div");
+        card.className = 'cardInfo';
         productWrapper.appendChild(card);
         let productImage = document.createElement("img");
+        productImage.className= 'image-style';
         productImage.src = product.URL;
-        let categoryPrice = document.createElement("div");
         card.appendChild(productImage);
-        let productName = document.createElement("span");
-        card.appendChild(productName);
         let productPrice = document.createElement("h3");
-        card.appendChild(productPrice);
+        productPrice.textContent = product.price;
         let productCategory = document.createElement("h3");
+        productCategory.textContent = product.category;
+        let productName = document.createElement("span");
+        productName.textContent = product.name;
+        card.appendChild(productName);
         card.appendChild(productCategory);
-        let descrption = document.createElement("div");
-        card.appendChild(descrption);
-        
+        card.appendChild(productPrice);
+        let cartIcon = document.createElement("i");
+        cartIcon.className = 'fas fa-cart-plus';
+        let cartButton = document.createElement("button")
+        cartButton.className = 'buttonStyle';
+        cartButton.appendChild(cartIcon);
+        card.appendChild(cartButton);
+        cartButton.addEventListener('click', function(){
+            cartItem.push(product);
+            console.log(cartItem);
+        })
+    });
+    let icon = document.getElementById('searchBtn');
+    icon.addEventListener('click', function(e){
+        e.preventDefault();
+        let item = document.getElementById("search-engine").value;
+        console.log(item);
+        let filterProducts = products.filter((product) => product.name == item);
+        console.log(filterProducts);
     })
     
-
-
